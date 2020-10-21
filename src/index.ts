cc424a0036befe9303f9310810d1517bf87415e7
wskurniawan <kurniawan.wisnu23@gmail.com>
2020-10-21 19:15:17 +0700

    add initApp async function

--- src/index.ts
+++ src/index.ts
@@ -7,30 +7,34 @@ dotenv.config()
 import express from 'express'
 import bodyParser from 'body-parser'
 
-const app = express()
+async function initApp() {
+  const app = express()
 
-app.use(bodyParser.json())
+  app.use(bodyParser.json())
 
-app.post('/customer', function(req, res, next) {
+  app.post('/customer', function(req, res, next) {
 
-})
+  })
 
-app.get('/customer', function(req, res, next) {
+  app.get('/customer', function(req, res, next) {
 
-})
+  })
 
-app.get('/cutomer/:id', function(req, res, next) {
+  app.get('/cutomer/:id', function(req, res, next) {
 
-})
+  })
 
-app.put('/customer', function(req, res, next) {
+  app.put('/customer', function(req, res, next) {
 
-})
+  })
 
-app.delete('/customer', function(req, res, next) {
+  app.delete('/customer', function(req, res, next) {
 
-})
+  })
 
-app.listen(process.env.PORT || 8000, () => {
-  console.log(`App listen on port ${ process.env.PORT || 8000 }`)
-})
+  app.listen(process.env.PORT || 8000, () => {
+    console.log(`App listen on port ${ process.env.PORT || 8000 }`)
+  })
+}
+
+initApp()
